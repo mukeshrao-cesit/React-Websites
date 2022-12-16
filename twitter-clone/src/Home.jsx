0b51378abec714f4d2edb4714f31aeaf8c275f18
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2022-12-16 17:16:26 +0530

    menubar

--- twitter-clone/src/Home.jsx
+++ twitter-clone/src/Home.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./style/Home.css";
+// import MidComp from "./MiddleComponent";
+import { MenuBar } from "./MenuBar";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="HomePage">
+      <div className="Row">
+        <div className="Col col-1">
+          <MenuBar />
+        </div>
+        <div className="Col col-2">
+          <BrowserRouter>
+            <Routes>
+              {/* <Route path="/Feeds" element={<MidComp />} /> */}
+              {/* <Route path="/Threads" element={<Threads />} /> */}
+              {/* <Route path="/" element={<Navigate to="/Feeds" />} /> */}
+              {/* <Route path="/Profile" element={<Profile />} /> */}
+            </Routes>
+          </BrowserRouter>
+        </div>
+        <div className="col-3">Last Component</div>
+      </div>
+    </div>
+  );
 };
---
+++ twitter-clone/src/MiddleComponent.jsx
@@ -0,0 +1,12 @@
+import Feeds from "./Feeds";
+const MidComp = () => {
+  return (
+    <div className="Tweet-body" id="tweetMainContainer">
+      {/* <Navbar /> */}
+      {/* <MyTweet /> */}
+      <Feeds />
+    </div>
+  );
+};
+
+export default MidComp;
